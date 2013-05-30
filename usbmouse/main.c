4409d907b8fb4fa9ea40bf73524a3df51329e3d0
lyyyuna <lyyyuna@gmail.com>
2013-05-30 21:39:24 +0800

    add d12 interrupt

--- usbmouse/main.c
+++ usbmouse/main.c
@@ -3,6 +3,8 @@
 #include "led.h"
 #include "uart.h"
 #include "pdiusbd12.h"
+#include "config.h"
+#include "usbcore.h"
 
 code uint8 HeadTable[][74] = {
 "********************************************************************\r\n",
@@ -19,15 +21,16 @@ void main(void)
 {
     uint8 i;
     uint16 id;
-    
+    uint8 InterruptSource;
+
     EA = 1;
     InitKeyboard();
     uart_init();
-    
+
     for (i=0; i<11; i++) {
         prints(HeadTable[i]);
     }
-    
+
     id = d12_readID();
     prints("Your D12 chip\'s ID is: ");
     printshortIntHex(id);
@@ -37,8 +40,41 @@ void main(void)
     else {
         prints(". ID is incorrect! What a pity!\r\n\r\n");
     }
-    
+
+    usb_disconnect();
+    usb_connect();
+    ConfigValue = 0;
+
     while(1) {
-    
+        if ( d12_getintpin() == 0) {
+            d12_writecommand(READ_INTERRUPT_REGISTER);
+            InterruptSource = d12_readbyte();
+            if (InterruptSource & 0x80)
+                usb_bussuspend();
+            if (InterruptSource & 0x40)
+                usb_busreset();
+            if (InterruptSource & 0x01)
+                usb_ep0out();
+            if (InterruptSource & 0x02)
+                usb_ep0in();
+            if (InterruptSource & 0x04)
+                usb_ep1out();
+            if (InterruptSource & 0x08)
+                usb_ep1in();
+            if (InterruptSource & 0x10)
+                usb_ep2out();
+            if (InterruptSource & 0x20)
+                usb_ep2in();
+        }
+        if (configvalue != 0) {
+            LEDs = ~KeyPress;
+            if (!ep1in_isbusy) {
+                KeyCanChange = 0;
+                if (KeyUp || KeyDown || KeyPress) {
+                    SendReport();
+                }
+                KeyCanChange = 1;
+            }
+        }
     }
 }
